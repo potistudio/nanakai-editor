53874a38f4c7d1adf1a9c6da73cba1f0025ff09d
POTI <potistudio2022@gmail.com>
2024-12-30 21:59:19 +0900

    👍 extract midi data as chart notes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
 import { invoke } from "@tauri-apps/api/core";
 import { Midi } from "@tonejs/midi";
 
-const chartMidi = await fetch ("./src/assets/chart.mid").then (r => r.arrayBuffer());
+const chartMidi = await fetch ("./src/assets/C0.mid").then (r => r.arrayBuffer());
+
+const FADER_COUNT = 5;
+const BASE_MIDI_TAP = 0;
+const BASE_MIDI_HOLD = 5;
+const BASE_MIDI_FADER = 12;
+
+type Note = {
+	time: number,
+	duration: number,
+	velocity: number,
+	pos: number,
+};
+
+type Midi = {
+	barCount: number,
+	duration: number,
+	notes: Note[],
+}
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
@@ -26,10 +44,65 @@ window.addEventListener("DOMContentLoaded", () => {
 
 (function init() {
 	const parsedMidi = parseMidi (new Uint8Array(chartMidi));
-	console.log (parsedMidi);
+
+	console.log ({
+		tapNotes: extractTapNotes (parsedMidi.notes),
+		holdNotes: extractHoldNotes (parsedMidi.notes),
+		faderEvents: extractFaderEvent (parsedMidi.notes),
+	});
 }());
+
+function extractTapNotes (_midi: Note[]): Note[][] {
+	let notes = [];
+
+	for (let i = 0; i < FADER_COUNT; i++) {
+		notes.push (_midi.filter (note => note.pos === BASE_MIDI_TAP + i));
+	}
+
+	return notes;
+}
+
+function extractHoldNotes (_notes: Note[]): Note[][] {
+	let notes = [];
+
+	for (let i = 0; i < FADER_COUNT; i++) {
+		notes.push (_notes.filter (note => note.pos === BASE_MIDI_HOLD + i));
+	}
+
+	return notes;
+}
+
+function extractFaderEvent (_notes: Note[]): Note[][] {
+	const FADER_TICK = 5;
+	const faders: Note[][] = [];
+
+	for (let i = 0; i < FADER_COUNT; i++) {
+		const base = BASE_MIDI_FADER + i * FADER_TICK;
+		faders.push (_notes.filter(note => note.pos >= base && note.pos < base + FADER_TICK));
+	}
+
+	return faders;
+}
 
 function parseMidi (_midiBuffer: Uint8Array): Midi {
 	const parsedMidi = new Midi (_midiBuffer);
-	return parsedMidi;
+
+	const pulses = parsedMidi.header.ppq;  // 96
+	const durationTicks = parsedMidi.durationTicks;  // 384
+	const barCount = Math.ceil (durationTicks / pulses);  // 4
+	const duration = parsedMidi.duration;  // bpm / 60 * barCount * 4
+	const notes = parsedMidi.tracks[0].notes.map (note => {
+		return {
+			time: note.time,
+			duration: note.duration,
+			velocity: note.velocity,
+			pos: note.midi,
+		}
+	});
+
+	return {
+		barCount,
+		duration,
+		notes,
+	};
 }
